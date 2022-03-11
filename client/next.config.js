8be38a393f56c0f6e5c84d21f3bd10be5fb0acf6
shu20031026 <shu20031026@gmail.com>
2022-03-12 00:13:28 +0900

    add(next-ui): document.tsxの設定

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,16 @@
-module.exports = {
-  reactStrictMode: true,
-};
+// module.exports = {
+//   reactStrictMode: true,
+// };
 
+module.exports = {
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 200,
+      };
+    }
+
+    return config;
+  },
+};
