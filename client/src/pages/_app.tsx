fc2e7c4787c06704389a7221d8727a13b2324fca
shu20031026 <shu20031026@gmail.com>
2022-03-12 15:54:19 +0900

    add(client): google oauthの設定

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
+import { SessionProvider } from "next-auth/react";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <NextUIProvider>
-      <Component {...pageProps} />
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </NextUIProvider>
   );
 };
