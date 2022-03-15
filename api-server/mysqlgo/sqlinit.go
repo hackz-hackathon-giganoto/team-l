a2cc560c90d120743553ae203550f6a2033943ae
shu20031026 <shu20031026@gmail.com>
2022-03-15 14:23:48 +0900

    fix:コメントアウトの削除

--- api-server/mysqlgo/sqlinit.go
+++ api-server/mysqlgo/sqlinit.go
@@ -21,47 +21,9 @@ func CheckError(err error) {
 
 func InitDB() string {
 	LoadEnv()
-
-	// 接続文字列を初期化します。
+	// 接続文字列の初期化
 	var connectionString = fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?allowNativePasswords=true", user, password, host, database)
 	return connectionString
-
-	// // 接続オブジェクトを初期化する。
-	// db, err := sql.Open("mysql", connectionString)
-	// CheckError(err)
-	// defer db.Close()
-
-	// err = db.Ping()
-	// CheckError(err)
-	// fmt.Println("Successfully created connection to database.")
-
-	// // 同名の以前のテーブルがあれば削除する。
-	// _, err = db.Exec("DROP TABLE IF EXISTS inventory;")
-	// CheckError(err)
-	// fmt.Println("Finished dropping table (if existed).")
-
-	// // テーブルを作成します。
-	// _, err = db.Exec("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);")
-	// CheckError(err)
-	// fmt.Println("Finished creating table.")
-
-	// // テーブルにデータを挿入する。
-	// sqlStatement, err := db.Prepare("INSERT INTO inventory (name, quantity) VALUES (?, ?);")
-	// res, err := sqlStatement.Exec("banana", 150)
-	// CheckError(err)
-	// rowCount, err := res.RowsAffected()
-	// fmt.Printf("Inserted %d row(s) of data.\n", rowCount)
-
-	// res, err = sqlStatement.Exec("orange", 154)
-	// CheckError(err)
-	// rowCount, err = res.RowsAffected()
-	// fmt.Printf("Inserted %d row(s) of data.\n", rowCount)
-
-	// res, err = sqlStatement.Exec("apple", 100)
-	// CheckError(err)
-	// rowCount, err = res.RowsAffected()
-	// fmt.Printf("Inserted %d row(s) of data.\n", rowCount)
-
 }
 
 func LoadEnv() {
@@ -69,16 +31,11 @@ func LoadEnv() {
 	err := godotenv.Load(".env")
 
 	if err != nil {
-		fmt.Printf("faile read env: %v", err)
+		fmt.Printf("fail read env: %v", err)
 	}
 
 	host = os.Getenv("GIGANOTO_HOST")
 	database = os.Getenv("GIGANOTO_DATABASE")
 	user = os.Getenv("GIGANOTO_USER")
 	password = os.Getenv("GIGANOTO_PASSWORD")
-
-	// fmt.Println(host)
-	// fmt.Println(database)
-	// fmt.Println(user)
-	// fmt.Println(password)
 }
