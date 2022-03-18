d91a762dc96992aa8c4d36b0c1a0016e20fbe6dc
shu20031026 <shu20031026@gmail.com>
2022-03-18 22:59:59 +0900

    fix: ハンドラのファイル分割

---
+++ api-server/booksArticleHandler/postHandler.go
@@ -0,0 +1,61 @@
+package booksArticleHandler
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"main/mysqlgo"
+	"main/tools"
+
+	"github.com/labstack/echo/v4"
+)
+
+// 投稿(POST)
+func PostArticleHandler(c echo.Context) error {
+	post := new(PostArticle)
+	userId := c.Param("id")
+
+	if err := c.Bind(post); err != nil {
+		return err
+	}
+	var lend string
+
+	articleId := tools.GenId()
+
+	isbn := post.Isbn
+	article := post.Article
+	if post.Lend {
+		lend = "true"
+	} else {
+		lend = "false"
+	}
+
+	//接続文字列を初期化します。
+	var connectionString = mysqlgo.InitDB()
+
+	// 接続オブジェクトを初期化する。
+	db, err := sql.Open("mysql", connectionString)
+	mysqlgo.CheckError(err)
+
+	defer db.Close()
+
+	tx, _ := db.Begin()
+
+	// テーブルにデータを挿入する。
+	sqlStatement, err := tx.Prepare(
+		"INSERT INTO books_article (article_id, user_id, isbn, article, lend) VALUES (?, ?, ?, ?, ?);")
+	res, err := sqlStatement.Exec(articleId, userId, isbn, article, lend)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	rowCount, err := res.RowsAffected()
+	fmt.Printf("Inserted %d row(s) of data.\n", rowCount)
+
+	tx.Commit()
+	return c.JSON(http.StatusCreated, "ok")
+}
